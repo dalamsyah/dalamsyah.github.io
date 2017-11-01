67a3b8b53f4cb2d13479b007d04d6b0e716579ad
dalamsyah <dimasalamsyah0712@gmail.com>
2017-11-01 09:09:58 +0700

    Update app.js

--- todo_angular/js/app.js
+++ todo_angular/js/app.js
@@ -9,12 +9,6 @@ app.controller('AppCtrl', function ($scope, $timeout, $mdSidenav, $log, $firebas
     $mdSidenav(menuId).toggle();
   };
 
-  var removeFunction = $mdTheming.setBrowserColor({
-    theme: 'myTheme', // Default is 'default'
-    palette: 'accent', // Default is 'primary', any basic material palette and extended palettes are available
-    hue: '200' // Default is '800'
-  });
-
   $scope.$on('$destroy', function () {
     removeFunction(); // COMPLETELY removes the browser color
   });
